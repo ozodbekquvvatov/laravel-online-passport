85c2b395ccaddb6ade6312d9e256adc8126bd4a0
Ozodbek <email@misol.com>
2025-04-03 20:31:50 +0500

    Add middleware to handle authentication checks

--- app/Http/Middleware/AuthMiddleware.php
+++ app/Http/Middleware/AuthMiddleware.php
@@ -17,6 +17,7 @@ class AuthMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
+        
 
         if (!Auth::check()) {
             return redirect()->route('users.index'); 
