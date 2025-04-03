b33196807d56898bf7162772a5432200c1526de9
Ozodbek <email@misol.com>
2025-04-03 20:25:01 +0500

    Add passport deletion with authorization check

--- app/Http/Controllers/PassportController.php
+++ app/Http/Controllers/PassportController.php
@@ -14,6 +14,7 @@ class PassportController extends Controller
     /**
      * Display a listing of the resource.
      */
+    
     public function index()
     {
              $passport = Auth::user()->passport; 
